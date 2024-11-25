96222ccff036a56e7e179fee88bd2c76a93b73d3
“Numan-star” <5125.2019.gct@gmail.com>
2024-11-25 23:11:22 +0500

    add db status

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import path from 'path';
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      oracledb: path.resolve(__dirname, 'node_modules/oracledb'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
