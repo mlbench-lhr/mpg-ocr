af01067ed09f3bddc133160ae0f1077fa1703066
“Numan-star” <5125.2019.gct@gmail.com>
2024-12-15 22:48:44 +0500

    run jobs

--- src/app/api/jobs/add-job/route.ts
+++ src/app/api/jobs/add-job/route.ts
@@ -196,7 +196,7 @@ export async function GET(req: Request) {
     const jobsCollection = await getJobsCollection();
     const url = new URL(req.url);
     const page = parseInt(url.searchParams.get("page") || "1", 10);
-    const limit = parseInt(url.searchParams.get("limit") || "3", 10);
+    const limit = parseInt(url.searchParams.get("limit") || "10", 10);
     const skip = (page - 1) * limit;
     const searchQuery = url.searchParams.get("search")?.trim().toLowerCase() || "";
 
