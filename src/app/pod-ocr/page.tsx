770c228ade8f8a8910ad3688c025ddf6651ab3d1
Ammar <mlbenchpvtltd@gmail.com>
2025-06-12 17:39:46 +0500

    Add filter section in pod oracle data tab

--- src/app/pod-ocr/page.tsx
+++ src/app/pod-ocr/page.tsx
@@ -8,6 +8,7 @@ import Spinner from "../components/Spinner";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import axios from "axios";
+import PODFilter from "../components/PODFilter";
 
 type Log = {
   FILE_ID: string;
@@ -39,13 +40,55 @@ export default function Page() {
   const [podData, setPodData] = useState<Log[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [applyFilters, setApplyFilters] = useState(false);
+  const [filters, setFilters] = useState({
+    file_id: "",
+    crtd_usr_cd: "",
+    crtd_dtt: "",
+    sent_file_dtt: "",
+    ocr_bolno: "",
+    ocr_issqty: "",
+    ocr_rcvqty: "",
+    ocr_stmp_sign: "",
+    ocr_symt_none: "",
+    ocr_symt_damg: "",
+    ocr_symt_shrt: "",
+    ocr_symt_orvg: "",
+    ocr_symt_refs: "",
+    ocr_symt_seal: "",
+    recv_data_dtt: "",
+    uptd_usr_cd: "",
+    uptd_dtt: "",
+    ocr_stmp_pod_dtt: "",
+    rnum: "",
+  });
   const router = useRouter();
+  const [filtersApplied, setFiltersApplied] = useState(false);
+
+  const handleApplyFilters = () => {
+    const isAnyFilterSet = Object.values(filters).some((val) => val !== "");
+    setFiltersApplied(isAnyFilterSet);
+
+    if (isAnyFilterSet) {
+      setApplyFilters(true); // this will trigger fetchPodData via useEffect
+    }
+  };
+
+  const handleResetFilters = () => {
+    setFilters(
+      (prev) =>
+        Object.fromEntries(
+          Object.keys(prev).map((key) => [key, ""])
+        ) as typeof filters
+    );
+    setFiltersApplied(false);
+    setApplyFilters(false); // prevents unnecessary fetch
+  };
 
   useEffect(() => {
-    const fullUrl = window.location.href; // includes protocol, hostname, port, and path
-    console.log(fullUrl);
-  }, []);
-
+    const isAny = Object.values(filters).some((v) => v !== "");
+    if (!isAny) setFiltersApplied(false);
+  }, [filters]);
   useEffect(() => {
     const token = localStorage.getItem("token");
 
@@ -99,10 +142,16 @@ export default function Page() {
     try {
       setLoadingTable(true);
 
-      const searchParam = searchQuery
-        ? `&search=${encodeURIComponent(searchQuery)}`
-        : "";
+      // const searchParam = searchQuery
+      //   ? `&search=${encodeURIComponent(searchQuery)}`
+      //   : "";
 
+      const searchParam = Object.entries(filters)
+        .filter(([_, value]) => value !== "")
+        .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+        .join("&");
+
+      console.log("search params-> ", searchParam);
       const response = await axios.get(
         `/api/oracle-data?page=${currentPage}${searchParam}`
       );
@@ -119,8 +168,10 @@ export default function Page() {
   }, [currentPage, searchQuery]);
 
   useEffect(() => {
-    fetchPodData();
-  }, [currentPage, fetchPodData, searchQuery]);
+    if (applyFilters) {
+      fetchPodData();
+    }
+  }, [applyFilters, currentPage, fetchPodData]);
 
   if (!isAuthenticated) return <p>Access Denied. Redirecting...</p>;
 
@@ -146,6 +197,28 @@ export default function Page() {
           }
           buttonContent={""}
         />
+        <div>
+          <PODFilter filters={filters} setFilters={setFilters} />
+        </div>
+        <div className="flex justify-end items-center gap-2 col-span-3">
+          <button
+            onClick={handleResetFilters}
+            disabled={!filtersApplied}
+            className={`underline ${
+              !filtersApplied
+                ? "text-gray-400 cursor-not-allowed"
+                : "text-[#005B97] cursor-pointer"
+            }`}
+          >
+            Reset Filters
+          </button>
+          <button
+            onClick={handleApplyFilters}
+            className="px-4 py-2 rounded-lg bg-[#005B97] text-white hover:bg-[#2270a3]"
+          >
+            Apply Filters
+          </button>
+        </div>
         <div className="flex-1 p-4 bg-white">
           {loadingTable ? (
             <div className="flex justify-center items-center">
@@ -163,164 +236,166 @@ export default function Page() {
               />
             </div>
           ) : (
-            <div className="w-full overflow-x-auto grid">
-              <table className="min-w-full bg-white border-gray-300">
-                <thead>
-                  <tr className="text-xl text-gray-800">
-                    <th className="py-2 px-4 border-b text-start font-medium">
-                      File ID
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      CRTD_USR_CD
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      CRTD_DTT
-                    </th>
+            <>
+              <div className="w-full overflow-x-auto grid">
+                <table className="min-w-full bg-white border-gray-300">
+                  <thead>
+                    <tr className="text-xl text-gray-800">
+                      <th className="py-2 px-4 border-b text-start font-medium">
+                        File ID
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        CRTD_USR_CD
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        CRTD_DTT
+                      </th>
 
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      SENT_FILE_DTT
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_BOLNO
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_ISSQTY
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_RCVQTY
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_STMP_SIGN
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_SYMT_NONE
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_SYMT_DAMG
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_SYMT_SHRT
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_SYMT_ORVG
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_SYMT_REFS
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_SYMT_SEAL
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      RECV_DATA_DTT
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      UPTD_USR_CD
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      UPTD_DTT
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      OCR_STMP_POD_DTT
-                    </th>
-                    <th className="py-2 px-4 border-b text-center font-medium">
-                      RNUM
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {podData.map((podData: Log, index: number) => (
-                    <tr key={index} className="text-gray-600">
-                      <td className="py-1 px-4 border-b text-start text-lg font-medium">
-                        {podData.FILE_ID}
-                      </td>
-
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.CRTD_USR_CD}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center text-gray-500">
-                        {new Date(podData.CRTD_DTT).toLocaleDateString(
-                          "en-US",
-                          {
-                            year: "numeric",
-                            month: "short",
-                            day: "numeric",
-                          }
-                        )}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center text-gray-500">
-                        {new Date(podData.SENT_FILE_DTT).toLocaleDateString(
-                          "en-US",
-                          {
-                            year: "numeric",
-                            month: "short",
-                            day: "numeric",
-                          }
-                        )}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_BOLNO}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_ISSQTY}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_RCVQTY}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_STMP_SIGN}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_SYMT_NONE}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_SYMT_DAMG}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_SYMT_SHRT}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_SYMT_ORVG}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_SYMT_REFS}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.OCR_SYMT_SEAL}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center text-gray-500">
-                        {new Date(podData.RECV_DATA_DTT).toLocaleDateString(
-                          "en-US",
-                          {
-                            year: "numeric",
-                            month: "short",
-                            day: "numeric",
-                          }
-                        )}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.UPTD_USR_CD}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center text-gray-500">
-                        {new Date(podData.UPTD_DTT).toLocaleDateString(
-                          "en-US",
-                          {
-                            year: "numeric",
-                            month: "short",
-                            day: "numeric",
-                          }
-                        )}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center text-gray-500">
-                        {podData.OCR_STMP_POD_DTT}
-                      </td>
-                      <td className="py-1 px-4 border-b text-center">
-                        {podData.RNUM}
-                      </td>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        SENT_FILE_DTT
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_BOLNO
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_ISSQTY
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_RCVQTY
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_STMP_SIGN
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_SYMT_NONE
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_SYMT_DAMG
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_SYMT_SHRT
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_SYMT_ORVG
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_SYMT_REFS
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_SYMT_SEAL
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        RECV_DATA_DTT
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        UPTD_USR_CD
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        UPTD_DTT
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        OCR_STMP_POD_DTT
+                      </th>
+                      <th className="py-2 px-4 border-b text-center font-medium">
+                        RNUM
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                  </thead>
+                  <tbody>
+                    {podData.map((podData: Log, index: number) => (
+                      <tr key={index} className="text-gray-600">
+                        <td className="py-1 px-4 border-b text-start text-lg font-medium">
+                          {podData.FILE_ID}
+                        </td>
+
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.CRTD_USR_CD}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center text-gray-500">
+                          {new Date(podData.CRTD_DTT).toLocaleDateString(
+                            "en-US",
+                            {
+                              year: "numeric",
+                              month: "short",
+                              day: "numeric",
+                            }
+                          )}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center text-gray-500">
+                          {new Date(podData.SENT_FILE_DTT).toLocaleDateString(
+                            "en-US",
+                            {
+                              year: "numeric",
+                              month: "short",
+                              day: "numeric",
+                            }
+                          )}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_BOLNO}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_ISSQTY}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_RCVQTY}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_STMP_SIGN}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_SYMT_NONE}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_SYMT_DAMG}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_SYMT_SHRT}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_SYMT_ORVG}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_SYMT_REFS}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.OCR_SYMT_SEAL}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center text-gray-500">
+                          {new Date(podData.RECV_DATA_DTT).toLocaleDateString(
+                            "en-US",
+                            {
+                              year: "numeric",
+                              month: "short",
+                              day: "numeric",
+                            }
+                          )}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.UPTD_USR_CD}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center text-gray-500">
+                          {new Date(podData.UPTD_DTT).toLocaleDateString(
+                            "en-US",
+                            {
+                              year: "numeric",
+                              month: "short",
+                              day: "numeric",
+                            }
+                          )}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center text-gray-500">
+                          {podData.OCR_STMP_POD_DTT}
+                        </td>
+                        <td className="py-1 px-4 border-b text-center">
+                          {podData.RNUM}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </>
           )}
 
           {loadingTable || totalPages === 0 || podData.length === 0 ? null : (
